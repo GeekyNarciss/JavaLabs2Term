cc23bb6787bb5791de691f312252aa4abfdf2678
Artemiy <GeekyNarciss@users.noreply.github.com>
2020-06-02 13:50:03 +0300

    commit with lab7

---
+++ lab7/src/bsu/rfe/java/group7/lab7/Nastachenko/varA/CorrectIP.java
@@ -0,0 +1,16 @@
+package bsu.rfe.java.group7.lab7.Nastachenko.varA;
+
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
+public class CorrectIP {
+    public static boolean isCorrectIpAdress(String ip) {
+        try {
+            return !Inet4Address.getByName(ip)
+                    .getHostAddress().equals(ip);
+        } catch (UnknownHostException ex) {
+            return true;
+        }
+    }
+}
